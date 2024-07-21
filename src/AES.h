b399df85e1a176813dab8d626776b7c4907a25ec
Juliet Bradford <julietallisonbradford@gmail.com>
2024-07-21 04:37:16 -0400

    updated header, gonna add comments soon then probably leave this be for a while

--- src/AES.h
+++ src/AES.h
@@ -1,7 +1,7 @@
 #ifndef AES_H
 #define AES_H
 
-void cipher (uint8_t[4][4], uint8_t[4][4], uint32_t *, unsigned int, bool);
-void invCipher (uint8_t[4][4], uint8_t[4][4], uint32_t *, unsigned int, bool;
+void encryptBlock (uint8_t[4][4], uint8_t[4][4], uint32_t *, unsigned int, bool, FILE *);
+void decryptBlock (uint8_t[4][4], uint8_t[4][4], uint32_t *, unsigned int, bool, FILE *);
 
 #endif
